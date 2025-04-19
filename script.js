cebb0a3d13d261a74f3f4b8f3101a55bdb6133ba
Khadulah <115075898+Khadulah@users.noreply.github.com>
2025-04-19 23:26:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple alert on contact button
+document.getElementById("contact-btn").addEventListener("click", function () {
+    alert("Thanks for reaching out! I'll get back to you soon.");
+  });
